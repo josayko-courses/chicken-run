2ec333062fee286aa01a3e7902f39df846b34e5a
Jonny Saykosy <josayko@pm.me>
2022-07-05 20:22:42 +0200

    add TypeORM and connect to postgres db

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ChickensModule } from './chickens/chickens.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ChickensModule],
+  imports: [
+    ChickensModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'docker',
+      password: 'docker',
+      database: 'chicken_run',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
