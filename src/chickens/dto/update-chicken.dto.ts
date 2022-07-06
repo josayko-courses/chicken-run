404fea33cd5485c248785ec0847bf06a7e08b4fa
Jonny Saykosy <josayko@pm.me>
2022-07-06 12:46:06 +0200

    explicitly describe updateChickenDto instead of extends from PartialType. Fix steps reset when update with PATCH.

--- src/chickens/dto/update-chicken.dto.ts
+++ src/chickens/dto/update-chicken.dto.ts
@@ -1,5 +1,39 @@
 import { PartialType } from '@nestjs/swagger';
-import { CreateChickenDto } from './create-chicken.dto';
+import { Type } from 'class-transformer';
+import {
+  IsString,
+  IsDateString,
+  IsOptional,
+  IsNumber,
+  IsBoolean,
+  ValidateNested,
+} from 'class-validator';
+import { Farmyard } from '../entities/farmyard.entity';
 
 // PartialType returns the type passed into it and mark all the fields as optional
-export class UpdateChickenDto extends PartialType(CreateChickenDto) {}
+export class UpdateChickenDto {
+  @IsString()
+  @IsOptional()
+  readonly name: string;
+
+  @IsDateString()
+  @IsOptional()
+  readonly birthday;
+
+  @IsNumber()
+  @IsOptional()
+  readonly weight: number;
+
+  @IsNumber()
+  @IsOptional()
+  readonly steps: number;
+
+  @IsBoolean()
+  @IsOptional()
+  readonly isRunning;
+
+  @IsOptional()
+  @Type(() => Farmyard)
+  @ValidateNested({ each: true })
+  readonly farmyard: Farmyard;
+}
